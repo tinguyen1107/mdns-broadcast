8261cc4b240b0b7d6747ef3427bd12d0b60f2313
NguyenTrongTin <ntrongtin11702@gmail.com>
2023-12-20 15:28:15 +0700

    test: mdns

---
+++ go.mod
@@ -0,0 +1,10 @@
+module example.com/mdns-broadcast
+
+go 1.21.0
+
+require (
+	github.com/hashicorp/mdns v1.0.5 // indirect
+	github.com/miekg/dns v1.1.41 // indirect
+	golang.org/x/net v0.0.0-20210410081132-afb366fc7cd1 // indirect
+	golang.org/x/sys v0.0.0-20210330210617-4fbd30eecc44 // indirect
+)
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+
+	"github.com/hashicorp/mdns"
+)
+
+func main() {
+	// Setup our service export
+	host, _ := os.Hostname()
+	info := []string{"My awesome service"}
+	service, _ := mdns.NewMDNSService(host, "_foobar._tcp", "", "", 8000, nil, info)
+
+	// Create the mDNS server, defer shutdown
+	server, _ := mdns.NewServer(&mdns.Config{Zone: service})
+	defer server.Shutdown()
+}
